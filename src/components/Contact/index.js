5b120fc0447fc1d8d8a54eccb3b769d6e0207ded
galenomoon <gui.galenocover@gmail.com>
2022-11-16 15:23:01 -0300

    hover on contacts

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Contact({ icon = 'no icon', link = '#' }) {
   return (
-    <a href={link} className='bg-gradient-to-r from-black to-[#072C27] flex items-center justify-center border-r-[4px] hover:border-r-[7px] w-[60px]  duration-200  border-secondary p-3'>
+    <a href={link} className='bg-gradient-to-r from-black to-[#072C27] flex items-center justify-center hover:translate-x-1 border-r-[4px] w-[60px]  duration-200  border-secondary p-3'>
       {icon}
     </a>
   )
